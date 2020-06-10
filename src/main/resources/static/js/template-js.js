d2a3cdc07aeb347c7257bc23a45bee242ca73622
AllanInComp <35985623+AllanInComp@users.noreply.github.com>
2020-06-10 16:54:54 -0400

    a little responsive

---
+++ src/main/resources/static/js/template-js.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+
+
+    // Side Navbar toggle button
+    $('#toggle-btn').on('click', function (e) {
+        // blocks default event handling
+        e.preventDefault();
+
+
+        if ($(window).outerWidth() > 1194) {
+            // becomes a little smaller on larger screens
+            $('nav.side-navbar').toggleClass('shrink');
+            $('.content').toggleClass('active');
+        } else {
+            $('nav.side-navbar').toggleClass('show-sm');
+            $('.content').toggleClass('active-sm');
+        }
+
+        // Enable tooltips
+        $('[data-toggle="tooltip"]').tooltip()
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
